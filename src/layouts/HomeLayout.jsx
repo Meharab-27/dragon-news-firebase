765ac931a6ef4cde394830840f436b9996b58af8
Meharab-27 <meharabrahman100115@gmail.com>
2025-10-19 00:06:43 +0600

    sticky scroll

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -11,21 +11,21 @@ const HomeLayout = () => {
     <div>
       <header>
         <Header></Header>
-        <section className="w-11/12 mx-auto my-3">
+        <section className="w-10/12 mx-auto my-3">
           <LatestNews></LatestNews>
         </section>
-        <nav className="w-11/12 mx-auto my-3">
+        <nav className="w-10/12 mx-auto my-3">
           <Navbar></Navbar>
         </nav>
       </header>
-      <main className="w-11/12 mx-auto my-3  grid grid-cols-12 gap-5">
-        <aside className="col-span-3">
+      <main className="w-10/12 mx-auto my-3  grid grid-cols-12 gap-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <LeftAside></LeftAside>
         </aside>
         <section className="main col-span-6">
           <Outlet></Outlet>
         </section>
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-0 h-fit">
           <RighAside></RighAside>
         </aside>
       </main>
